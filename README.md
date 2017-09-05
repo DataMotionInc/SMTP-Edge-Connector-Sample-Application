7600b0794376f5eece0c27e78f48ce28e22bec16
Kyle Corsi <kylec@datamotion.com>
2017-09-05 15:36:39 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# SMTP Edge Connector - Sample Application
+
+[![DataMotion Health Logo](https://www.datamotionhealth.com/wp-content/themes/dhealth/images/logo.png)](https://www.datamotionhealth.com)
+
+## Description
+
+This is a C# Sample Application for the DataMotion Health (DataMotion Direct), SMTP Edge Connector. This Edge Connector is required for some certifications. This Edge Connector is being provided as a secure SMTP/POP3 service, at this time only. This code is simple in nature, and is present to provide a basic implementation. This sample code is for illustration purposes and is not part of any supported product.
+
+## Pre-Requisites
+
+1. It is assumed you have a valid DataMotion Health (DataMotion Direct) account
+1. It is assumed you have a valid DataMotion SMTP Edge Connector account
+
+>If you need assistance with either of these pre-requisites please contact sales@datamotion.com
+
+## Dependencies
+
+1. .NET Framework 4.5.2
+1. OpenPop.NET 2.0.6.1120
+
+## Installation
+
+1. Clone the repository to your local disk
+1. Open the **SMTP Client Test.sln** file in Visual Studio to open the entire project
+
+## Use
+
+Simply run the program from within Visual Studio or you can run the built code in the **bin** subdirectory.
+
+You can also try out the published setup files in the **publish** subdirectory.
+
+## Author
+
+**Name:** Kyle Corsi
+
+**Title:** Software Engineer
+
+**Initial Commit Date:** 8/16/2017
---
+++ SMTP Client Test/Program.cs
@@ -0,0 +1,97 @@
+﻿using OpenPop.Mime;
+using OpenPop.Pop3;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Threading;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SMTP_Client_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SMTP Edge Connector Hostname
+            string hostname = "testing.datamotioncorp.com";
+
+            //SMTP Edge Connector Port
+            int port = 25;
+
+            //SMTP Edge Connector SSL
+            bool ssl = true;
+
+            //Enter your SMTP Edge Connector credentials
+            string username = "<username>";
+            string password = "<password>";
+
+            //Create thread for polling messages
+            Thread pollThread = StartTheThread(hostname, 995, ssl, username, password);
+            
+            //On testing SMTP Edge Connector system, ignore SSL error checks (you could also download and install the cert)
+            ServicePointManager.ServerCertificateValidationCallback = ((sender, certificate, chain, sslPolicyErros) => true);
+            var client = new SmtpClient(hostname, port);
+            client.UseDefaultCredentials = false;
+            client.EnableSsl = ssl;
+            
+            //Set SMTP Client's credentials
+            client.Credentials = new NetworkCredential(username, password);
+            
+            //Construct new MailMessage (from, to)
+            MailMessage message = new MailMessage(username, "<target>");
+            message.Body = "TEST MESSAGE";
+            message.Subject = "TEST FROM DM";
+            client.Send(message);
+            Console.ReadLine();
+        }
+
+        private static Thread StartTheThread(string hostname, int port, bool ssl, string username, string password)
+        {
+            //Create a new Thread for the FetchMessages function and start it
+            var t = new Thread(() => FetchMessages(hostname, port, ssl, username, password));
+            t.Start();
+            return t;
+        }
+
+        private static void FetchMessages(string host, int port, bool ssl, string username, string password)
+        {
+            //3rd party Pop3 library
+            using (Pop3Client client = new Pop3Client())
+            {
+                //Connect and authenticate to Pop3 specifying our own certificate validator function to bypass self signed certificate
+                client.Connect(host, port, ssl, 5000, 5000, certificateValidator:certificateValidator);
+                client.Authenticate(username, password);
+
+                //Get number of messages in the inbox
+                int messageCount = client.GetMessageCount();
+
+                //Create List of type Message to hold the messages
+                List<Message> allMessages = new List<Message>(messageCount);
+                if (messageCount == 0)
+                {
+                    //If there are no messages
+                    Console.WriteLine("No messages in inbox");
+                }
+                for(int i = messageCount; i > 0; i--)
+                {
+                    //For every message add it to our List
+                    allMessages.Add(client.GetMessage(i));
+                }
+
+                foreach(Message message in allMessages)
+                {
+                    //For every message in our list convert it to a .NET MailMessage and print the Subject line
+                    Console.WriteLine("Found Message:" + message.ToMailMessage().Subject);
+                }
+            }
+        }
+
+        //Certificate validator function to bypass self signed certificate
+        private static bool certificateValidator(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+    }
+}
